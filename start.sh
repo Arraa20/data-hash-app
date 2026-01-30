63c8a05e6209a491b1ffe3af10f30c4d5d65fe3e
Arraamuthan Arulanantham <arraamuthan.a@adaglobal.com>
2026-01-30 15:16:56 +0530

    Modified start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Starting FastAPI on port 8000..."
+echo "Starting FastAPI..."
 uvicorn main:app --host 0.0.0.0 --port 8000 &
+
+# Wait 3 seconds to make sure FastAPI is ready
+sleep 3
 
 echo "Starting Streamlit..."
 streamlit run frontend.py --server.port=$PORT --server.address=0.0.0.0
