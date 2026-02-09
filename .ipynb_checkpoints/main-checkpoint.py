0e51767eb0c7296225476103720c99b988a1a30c
Arraamuthan Arulanantham <arraamuthan.a@adaglobal.com>
2026-02-09 16:38:03 +0530

    Modified main.py to include meta

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -104,7 +104,7 @@ async def hash_csv(file: UploadFile = File(...)):
 # -------------------------
 # Hash & Push to Meta Endpoint
 # -------------------------
-@app.post("/hash_and_upload", dependencies=[Depends(verify_api_key)])
+@app.post("/hash_and_upload_csv", dependencies=[Depends(verify_api_key)])
 async def hash_and_upload(file: UploadFile = File(...)):
     if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Only CSV allowed")
